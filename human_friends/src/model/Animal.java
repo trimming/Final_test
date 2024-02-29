b89a6fe626e89080978c616ae05a035d2fe443bd
Alexey Golikov <lexgolikoff@gmail.com>
2024-02-29 21:15:05 +0300

    create package model

---
+++ human_friends/src/model/Animal.java
@@ -0,0 +1,31 @@
+package model;
+
+public abstract class Animal {
+    private int animalId;
+    private String name;
+    private String kindOfAnimal;
+
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    public void setAnimalId(int animalId) {
+        this.animalId = animalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKindOfAnimal() {
+        return kindOfAnimal;
+    }
+
+    public void setKindOfAnimal(String kindOfAnimal) {
+        this.kindOfAnimal = kindOfAnimal;
+    }
+}
---
+++ human_friends/src/model/Pet.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Pet extends Animal{
+    private LocalDate dateOfBirth;
+    private ArrayList<String> commands;
+
+
+}
